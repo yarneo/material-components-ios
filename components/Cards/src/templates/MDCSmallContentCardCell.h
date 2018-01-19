d5a7ab2b8542f6519279347f60c6d4eb37fcd6a3
Yarden Eitan <yar@google.com>
2018-01-19 13:50:19 -0500

    cards extra

---
+++ components/Cards/src/templates/MDCSmallContentCardCell.h
@@ -0,0 +1,12 @@
+//
+//  MDCSmallContentCardCell.h
+//  MaterialComponents
+//
+//  Created by Yarden Eitan on 1/19/18.
+//
+
+#import <MaterialComponents/MaterialComponents.h>
+
+@interface MDCSmallContentCardCell : MDCCollectionViewCardCell
+
+@end
---
+++ components/Cards/src/templates/MDCSmallContentCardCell.m
@@ -0,0 +1,12 @@
+//
+//  MDCSmallContentCardCell.m
+//  MaterialComponents
+//
+//  Created by Yarden Eitan on 1/19/18.
+//
+
+#import "MDCSmallContentCardCell.h"
+
+@implementation MDCSmallContentCardCell
+
+@end
